3a66cea8dfa4a4f7ad02983f5be50b1abc810341
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2023-12-12 20:43:28 -0300

    resolucao exercicio 2 semana 07

---
+++ modulo01/semana7/exer02/app.js
@@ -0,0 +1,23 @@
+const userNameInput = document.querySelector("#inputName");
+const botaoBuscaPerfil = document.querySelector("#btnBuscaPerfil");
+const perfil = document.querySelector("#span-perfil");
+const repositorios = document.querySelector("#span-repositorio");
+const avatar = document.querySelector("#span-avatar");
+
+
+async function buscaPefilGitHub() {
+    const userName = userNameInput.value;
+    try {
+        const url = `https://api.github.com/users/${userName}`;
+        const resposta = await fetch(url);
+        const dados = await resposta.json();
+
+        perfil.innerHTML = `<strong>${dados.login}</strong>`;
+        repositorios.innerHTML = `<strong>${dados.public_repos}</strong>`;
+        avatar.innerHTML = `<img src="${dados.avatar_url}" alt="Avatar">`; // Utilizando uma tag img para exibir a imagem
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+botaoBuscaPerfil.addEventListener("click", buscaPefilGitHub);
