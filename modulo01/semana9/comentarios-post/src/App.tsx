6e976adbc63140ebb287277faed523181e6f53f9
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2024-01-17 21:03:01 -0300

    resolucao do exercicios post com comentarios

---
+++ modulo01/semana9/comentarios-post/src/App.tsx
@@ -0,0 +1,13 @@
+import { Post } from './components/Post';
+
+function App() {
+
+  return (
+    <div >
+      <Post />
+    </div>
+
+  )
+}
+
+export default App
---
+++ modulo01/semana9/comentarios-post/src/components/Comment.tsx
@@ -0,0 +1,17 @@
+import styles from './Comments.module.css';
+
+interface CommentProps {
+    content: string;
+}
+
+export function Comment({content } : CommentProps) {
+    return (
+        <div className={styles.comment}>
+            <div className={styles.commentContent}>
+                <div className={styles.contentDiv}>
+                    <p>{content}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
