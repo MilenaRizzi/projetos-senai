b0a0ccf929f694f0797beff1c60009bc504c1912
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2024-02-22 19:57:19 -0300

    resolucao do exercicio 2

---
+++ modulo02/semana1/exercicio2/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        char letra = 'M';
+        int idade = 26;
+        double altura = 1.65;
+
+        System.out.println("Letra: " + letra);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+
+    }
+}
