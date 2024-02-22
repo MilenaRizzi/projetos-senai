549eddcc8815b39b710781d2ae298e7505a6888b
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2024-02-22 20:02:21 -0300

    resolucao exercicio 3

---
+++ modulo02/semana1/exercicio3/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        int var1 = 20;
+        double var2 = 10.5;
+        
+        double soma = (double) var1 + var2;
+        
+        System.out.println("A soma dos valores é: " + soma);
+    }
+}
