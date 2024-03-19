4e062dce0a9dccc412cd672739d4d119c299fe86
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2024-03-19 10:49:04 -0300

    Update TesteContaPoupanca.java

--- modulo02/semana4/exercicios/exer02/TesteContaPoupanca.java
+++ modulo02/semana4/exercicios/exer02/TesteContaPoupanca.java
@@ -2,9 +2,9 @@ package exer02;
 
 public class TesteContaPoupanca {
   public static void main(String[] args) {
-    ContaPoupanca minhaContaPoupanca = new ContaPoupanca(54321, 450, "Maria", 5000.0, 2.5);
-    System.out.println("Saldo inicial: " + minhaContaPoupanca.getSaldo());
-    minhaContaPoupanca.adicionarJuros();
-    System.out.println("Novo saldo após juros: " + minhaContaPoupanca.getSaldo());
+    ContaPoupanca contaPoupancaMaria = new ContaPoupanca(54321, 450, "Maria", 5000.0, 2.5);
+    System.out.println("Saldo inicial: " + contaPoupancaMaria.getSaldo());
+    contaPoupancaMaria.adicionarJuros();
+    System.out.println("Novo saldo após juros: " + contaPoupancaMaria.getSaldo());
   }
 }
