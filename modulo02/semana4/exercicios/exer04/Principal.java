c25d3c3abc853126fe24b108ee1a8abc4758111c
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2024-03-19 11:36:11 -0300

    Update Principal.java

--- modulo02/semana4/exercicios/exer04/Principal.java
+++ modulo02/semana4/exercicios/exer04/Principal.java
@@ -8,13 +8,13 @@ public class Principal {
     ContaPoupanca contaPoupancaMaria = new ContaPoupanca(54321, 450, "Maria", 5000.0, 2.5);
 
     contaPoupancaMaria.sacar(500);
-    System.out.println(contaPoupancaMaria.mostrarDados());
+    contaPoupancaMaria.mostrarDados();
 
     System.out.println();
-    
+
     ContaCorrente contaCorrenteJoao = new ContaCorrente(12345, 123, "João da Silva", 20000, 3.50);
 
     contaCorrenteJoao.depositar(1000);
-    System.out.println(contaCorrenteJoao.mostrarDados());
+    contaCorrenteJoao.mostrarDados();
   }
 }
