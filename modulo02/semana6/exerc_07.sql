c29cba58b7b4796a2aa88403ddfc3e490e5b6605
Your Name <milenaclararizzi@gmail.com>
2024-04-08 11:42:15 -0300

    Create exerc_07.sql

---
+++ modulo02/semana6/exerc_07.sql
@@ -0,0 +1,3 @@
+--Usando SQL, selecione todos os dados de todos os registros da tabela de heróis, para conferir o resultado final das transformações nos dados feito nos exercícios.
+
+SELECT * FROM herois;
