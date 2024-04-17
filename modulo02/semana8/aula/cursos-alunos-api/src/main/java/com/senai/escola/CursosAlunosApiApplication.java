f57485b1b9ace0507541a14cb662adfde57a1c98
Your Name <milenaclararizzi@gmail.com>
2024-04-17 16:58:47 -0300

    Create CursosAlunosApiApplication.java

---
+++ modulo02/semana8/aula/cursos-alunos-api/src/main/java/com/senai/escola/CursosAlunosApiApplication.java
@@ -0,0 +1,13 @@
+package com.senai.escola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosAlunosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosAlunosApiApplication.class, args);
+	}
+
+}
