bcb2d54ef2fb72cd31214b1fb9251b109ad163d1
Your Name <milenaclararizzi@gmail.com>
2024-04-17 10:10:29 -0300

    Create PatioAutomoveisAppApplication.java

---
+++ modulo02/simulado/patio_automoveis_app/src/main/java/com/senai/patio_automoveis/PatioAutomoveisAppApplication.java
@@ -0,0 +1,13 @@
+package com.senai.patio_automoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatioAutomoveisAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatioAutomoveisAppApplication.class, args);
+	}
+
+}
