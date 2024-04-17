5f010f984e880d90f7657ecad66ebdb66b1319a7
Your Name <milenaclararizzi@gmail.com>
2024-04-17 10:10:38 -0300

    Create PatioAutomoveisAppApplicationTests.java

---
+++ modulo02/simulado/patio_automoveis_app/src/test/java/com/senai/patio_automoveis/PatioAutomoveisAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.patio_automoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatioAutomoveisAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
