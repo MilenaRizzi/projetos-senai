2fe4df3a073365165a20bc1a26d17d0cf14afe8b
Your Name <milenaclararizzi@gmail.com>
2024-04-17 10:09:41 -0300

    Create HeroisApplication.java

---
+++ modulo02/semana7/mini-projeto/herois/src/main/java/com/senai/herois/HeroisApplication.java
@@ -0,0 +1,13 @@
+package com.senai.herois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroisApplication.class, args);
+	}
+
+}
