63c82420340fa502714d406fe330cabccbd272a6
Your Name <milenaclararizzi@gmail.com>
2024-04-17 11:55:55 -0300

    Update HeroiRepository.java

--- modulo02/semana7/mini-projeto/herois/src/main/java/com/senai/herois/repository/HeroiRepository.java
+++ modulo02/semana7/mini-projeto/herois/src/main/java/com/senai/herois/repository/HeroiRepository.java
@@ -7,6 +7,11 @@ import com.senai.herois.model.Heroi;
 
 public class HeroiRepository {
   private List<Heroi> herois = new ArrayList<>();
+  
+  public Heroi adicionar(Heroi heroi) {
+    herois.add(heroi);
+    return heroi;
+  }
 
   public List<Heroi> consultar() {
     return herois;
