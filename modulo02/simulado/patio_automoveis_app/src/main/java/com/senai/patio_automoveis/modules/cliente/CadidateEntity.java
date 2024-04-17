a66fc057d839b8ba6e3594abf7bbf4932605592a
Your Name <milenaclararizzi@gmail.com>
2024-04-17 10:10:25 -0300

    Create CadidateEntity.java

---
+++ modulo02/simulado/patio_automoveis_app/src/main/java/com/senai/patio_automoveis/modules/cliente/CadidateEntity.java
@@ -0,0 +1,11 @@
+package com.senai.patio_automoveis.modules.cliente;
+
+import java.util.UUID;
+
+import lombok.Data;
+
+@Data
+public class CadidateEntity {
+  private UUID id;
+  private String nome;
+}
