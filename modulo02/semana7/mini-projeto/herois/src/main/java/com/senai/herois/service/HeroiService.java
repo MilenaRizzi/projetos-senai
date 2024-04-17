aee51e7a45ea782b52e8596e1c43a8767c778972
Your Name <milenaclararizzi@gmail.com>
2024-04-17 11:57:26 -0300

    Update HeroiService.java

--- modulo02/semana7/mini-projeto/herois/src/main/java/com/senai/herois/service/HeroiService.java
+++ modulo02/semana7/mini-projeto/herois/src/main/java/com/senai/herois/service/HeroiService.java
@@ -3,10 +3,12 @@ package com.senai.herois.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.senai.herois.model.Heroi;
 import com.senai.herois.repository.HeroiRepository;
 
+@Service
 public class HeroiService {
   
   @Autowired
