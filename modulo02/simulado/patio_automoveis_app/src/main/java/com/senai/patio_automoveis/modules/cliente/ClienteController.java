573a0b47a2a458f97d73b09fba937e1e45f77861
Your Name <milenaclararizzi@gmail.com>
2024-04-17 10:10:27 -0300

    Create ClienteController.java

---
+++ modulo02/simulado/patio_automoveis_app/src/main/java/com/senai/patio_automoveis/modules/cliente/ClienteController.java
@@ -0,0 +1,10 @@
+package com.senai.patio_automoveis.modules.cliente;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("cliente")
+public class ClienteController {
+  
+}
