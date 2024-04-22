185196ed0429cac1d9105856ca05894ae75f4c9d
Your Name <milenaclararizzi@gmail.com>
2024-04-22 08:57:05 -0300

    Create PersonagensApiApplication.java

---
+++ modulo02/semana8/aula/personagens-api/src/main/java/com/senai/personagens/PersonagensApiApplication.java
@@ -0,0 +1,13 @@
+package com.senai.personagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonagensApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonagensApiApplication.class, args);
+	}
+
+}
