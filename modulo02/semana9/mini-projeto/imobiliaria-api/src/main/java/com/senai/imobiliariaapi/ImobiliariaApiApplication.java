bfbe4f3545ff19895084e34633a30f2d55d57bc8
Your Name <milenaclararizzi@gmail.com>
2024-04-26 20:29:39 -0300

    Create ImobiliariaApiApplication.java

---
+++ modulo02/semana9/mini-projeto/imobiliaria-api/src/main/java/com/senai/imobiliariaapi/ImobiliariaApiApplication.java
@@ -0,0 +1,12 @@
+package com.senai.imobiliariaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImobiliariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImobiliariaApiApplication.class, args);
+	}
+}
