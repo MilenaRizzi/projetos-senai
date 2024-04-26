9f3c6884abc9907ef77230cde1a21a02949953fa
Your Name <milenaclararizzi@gmail.com>
2024-04-26 20:29:08 -0300

    Create BibliotecaApiApplicationTests.java

---
+++ modulo02/semana9/aula/biblioteca-api/src/test/java/com/senai/biblioteca/BibliotecaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
