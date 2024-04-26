7f3a0b9765024b056d68844f8f56b6f0a921edb7
Your Name <milenaclararizzi@gmail.com>
2024-04-26 20:29:03 -0300

    Create BibliotecaApiApplication.java

---
+++ modulo02/semana9/aula/biblioteca-api/src/main/java/com/senai/biblioteca/BibliotecaApiApplication.java
@@ -0,0 +1,13 @@
+package com.senai.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaApiApplication.class, args);
+	}
+
+}
