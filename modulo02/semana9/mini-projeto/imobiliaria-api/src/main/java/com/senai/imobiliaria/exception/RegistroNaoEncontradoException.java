7f1278b6c06b68a4129cc2055feaa9a515f6bb7f
Your Name <milenaclararizzi@gmail.com>
2024-04-27 16:40:51 -0300

    delete

--- modulo02/semana9/mini-projeto/imobiliaria-api/src/main/java/com/senai/imobiliaria/exception/RegistroNaoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.senai.imobiliaria.exception;
-
-public class RegistroNaoEncontradoException extends RuntimeException{
-  public RegistroNaoEncontradoException(String mensagem) {
-    super(mensagem);
-}
-}
