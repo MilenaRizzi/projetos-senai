82b2c4e5e81bb537a61fc5e3a825cc0e1a088531
Your Name <milenaclararizzi@gmail.com>
2024-04-30 15:38:54 -0300

    Create WebSecurityConfig.java

---
+++ modulo02/semana10/parkingControl/src/main/java/com/example/parkingcontrol/config/security/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.parkingcontrol.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class WebSecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((request) -> request
+                .requestMatchers(HttpMethod.POST, "/user/**").permitAll()
+                .anyRequest()
+                .authenticated());
+    return http.build();
+  }
+
+}
