47a8e53c2914ae0b8d49500876389a430a9e012a
Your Name <milenaclararizzi@gmail.com>
2024-04-30 15:39:00 -0300

    Create ParkingSpotDto.java

---
+++ modulo02/semana10/parkingControl/src/main/java/com/example/parkingcontrol/dtos/ParkingSpotDto.java
@@ -0,0 +1,59 @@
+package com.example.parkingcontrol.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class ParkingSpotDto {
+    
+    @NotBlank
+    private String parkingSpotNumber;
+    @Size(max=7)
+    @NotBlank
+    private String licensePlateCar;
+    @NotBlank
+    private String responsibleName;
+    @NotBlank
+    private String apartment;
+    @NotBlank
+    private String block;
+
+    public String getParkingSpotNumber() {
+        return parkingSpotNumber;
+    }
+
+    public void setParkingSpotNumber(String parkingSpotNumber) {
+        this.parkingSpotNumber = parkingSpotNumber;
+    }
+
+    public String getLicensePlateCar() {
+        return licensePlateCar;
+    }
+
+    public void setLicensePlateCar(String licensePlateCar) {
+        this.licensePlateCar = licensePlateCar;
+    }
+
+    public String getResponsibleName() {
+        return responsibleName;
+    }
+
+    public void setResponsibleName(String responsibleName) {
+        this.responsibleName = responsibleName;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+
+    public String getBlock() {
+        return block;
+    }
+
+    public void setBlock(String block) {
+        this.block = block;
+    } 
+}
