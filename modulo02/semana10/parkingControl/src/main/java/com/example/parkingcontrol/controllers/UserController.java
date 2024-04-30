02a9fcf9d8b412be2ca03d2fe790bb548544f3cc
Your Name <milenaclararizzi@gmail.com>
2024-04-30 15:38:58 -0300

    Create UserController.java

---
+++ modulo02/semana10/parkingControl/src/main/java/com/example/parkingcontrol/controllers/UserController.java
@@ -0,0 +1,27 @@
+package com.example.parkingcontrol.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.parkingcontrol.models.UserModel;
+import com.example.parkingcontrol.services.UserService;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/user")
+public class UserController {
+  
+  @Autowired
+  UserService userService;
+
+  @PostMapping
+  public ResponseEntity<Object> saveUser(@RequestBody UserModel userModel){
+    return ResponseEntity.status(HttpStatus.CREATED).body(userService.saveUser(userModel));
+  }
+}
