3d2d9b2eae5238495bb2b06643cf2a2c9d1fb349
Your Name <milenaclararizzi@gmail.com>
2024-04-30 15:39:08 -0300

    Create UserModel.java

---
+++ modulo02/semana10/parkingControl/src/main/java/com/example/parkingcontrol/models/UserModel.java
@@ -0,0 +1,69 @@
+package com.example.parkingcontrol.models;
+
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_USER")
+public class UserModel {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID userId;
+
+  @Column(nullable = false, unique = true)
+  private String userName;
+
+  @Column(nullable = false)
+  private String password;
+
+  @ManyToMany
+  @JoinTable(name = "TB_USER_ROLES", 
+  joinColumns = @JoinColumn(name = "user_id"), 
+  inverseJoinColumns = @JoinColumn(name = "role_id"))
+  private List<RoleModel> roles;
+
+
+  public UUID getUserId() {
+    return this.userId;
+  }
+
+  public void setUserId(UUID userId) {
+    this.userId = userId;
+  }
+
+  public String getUserName() {
+    return this.userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public List<RoleModel> getRoles() {
+    return this.roles;
+  }
+
+  public void setRoles(List<RoleModel> roles) {
+    this.roles = roles;
+  }
+
+}
