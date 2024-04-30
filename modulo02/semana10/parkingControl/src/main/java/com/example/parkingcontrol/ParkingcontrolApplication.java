4e5ec30d69aae4665e1ecf0fca20024bfaa412e5
Your Name <milenaclararizzi@gmail.com>
2024-04-30 15:39:10 -0300

    Create ParkingcontrolApplication.java

---
+++ modulo02/semana10/parkingControl/src/main/java/com/example/parkingcontrol/ParkingcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.example.parkingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingcontrolApplication.class, args);
+	}
+
+}
