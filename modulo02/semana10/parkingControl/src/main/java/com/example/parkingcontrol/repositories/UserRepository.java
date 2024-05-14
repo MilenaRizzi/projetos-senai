b154559eff0e70051739e56425fc35d31f81f8ef
Your Name <milenaclararizzi@gmail.com>
2024-05-14 09:54:50 -0300

    Update UserRepository.java

--- modulo02/semana10/parkingControl/src/main/java/com/example/parkingcontrol/repositories/UserRepository.java
+++ modulo02/semana10/parkingControl/src/main/java/com/example/parkingcontrol/repositories/UserRepository.java
@@ -1,13 +1,18 @@
 package com.example.parkingcontrol.repositories;
+
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.parkingcontrol.models.UserModel;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, UUID>{
-  
-  UserModel findByUserName(String userName);
+    UserModel findByUsername(String username);
+
+    @Query("SELECT user FROM UserModel user JOIN FETCH user.roles WHERE user.username = :username")
+    UserModel findByUsernameFetchRoles(@Param("username") String username);
 }
