54118ad194f1388257be2543bae897f4ff622c3d
Your Name <milenaclararizzi@gmail.com>
2024-05-14 09:54:28 -0300

    Update ParkingSpotController.java

--- modulo02/semana10/parkingControl/src/main/java/com/example/parkingcontrol/controllers/ParkingSpotController.java
+++ modulo02/semana10/parkingControl/src/main/java/com/example/parkingcontrol/controllers/ParkingSpotController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,6 +36,7 @@ public class ParkingSpotController {
     @Autowired
     ParkingSpotService parkingSpotService;
 
+    @PreAuthorize("hasRole('PARKING_INSERT')")
     @PostMapping
     public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto) {
         if (parkingSpotService.existByLicensePlateCar(parkingSpotDto.getLicensePlateCar())) {
@@ -56,11 +58,13 @@ public class ParkingSpotController {
         return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
     }
 
+    @PreAuthorize("hasRole('PARKING_SELECT')")
     @GetMapping
     public ResponseEntity<Page<ParkingSpotModel>> getAllParkingSpots(@PageableDefault(page = 0, size = 10, sort="id", direction = Sort.Direction.ASC) Pageable pageable){
         return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll(pageable));
     }
 
+    @PreAuthorize("hasRole('PARKING_SELECT')")
     @GetMapping("/{id}")
     public ResponseEntity<Object> getOneParkingSpot(@PathVariable(value = "id") UUID id){
         Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
@@ -71,6 +75,7 @@ public class ParkingSpotController {
         return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
     }
     
+    @PreAuthorize("hasRole('PARKING_DELETE')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteParkingSpot(@PathVariable(value="id") UUID id){
         Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
@@ -84,8 +89,9 @@ public class ParkingSpotController {
         return ResponseEntity.status(HttpStatus.OK).body("Vaga de estacionamento excluída com sucesso!");
     }
 
+    @PreAuthorize("hasRole('PARKING_UPDATE')")
     @PutMapping("/{id}")
-    public ResponseEntity<Object> updateParkingSpot(@PathVariable(value = "id") UUID id, ParkingSpotDto parkingSpotDto){
+    public ResponseEntity<Object> updateParkingSpot(@PathVariable(value = "id") UUID id, @RequestBody @Valid ParkingSpotDto parkingSpotDto){
         Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
         
         if(!parkingSpotModelOptional.isPresent()){
