674375cac9fff3a229e228f7bb5cb9f0ca5cd8d3
Your Name <milenaclararizzi@gmail.com>
2024-05-14 09:54:22 -0300

    Update WebSecurityConfig.java

--- modulo02/semana10/parkingControl/src/main/java/com/example/parkingcontrol/config/security/WebSecurityConfig.java
+++ modulo02/semana10/parkingControl/src/main/java/com/example/parkingcontrol/config/security/WebSecurityConfig.java
@@ -1,25 +1,31 @@
 package com.example.parkingcontrol.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class WebSecurityConfig {
 
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests((request) -> request
-                .requestMatchers(HttpMethod.POST, "/user/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/parking-spot/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated());
-    return http.build();
-  }
+    @Autowired
+    CustomBasicAuthenticationFilter customBasicAuthenticationFilter;
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable())
+                    .authorizeHttpRequests((request) -> request
+                        .requestMatchers(HttpMethod.POST, "/user/**").permitAll()
+                        .anyRequest()
+                        .authenticated())
+                        .addFilterBefore(customBasicAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    } 
 }
